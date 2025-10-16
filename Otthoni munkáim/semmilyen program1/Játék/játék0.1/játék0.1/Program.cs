ba087899f4a874f68e64fc6528ee7df1f893661e
Dookusz <daniel.doka@umszki.hu>
2025-10-16 17:38:04 +0200

    Verzió 0.1

---
+++ Otthoni munkáim/semmilyen program1/Játék/játék0.1/játék0.1/Program.cs
@@ -0,0 +1,27 @@
+﻿//palya
+int hossz = 70;
+int magassag = 20;
+
+//játék háttere
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Clear();
+
+Console.WindowHeight = magassag;
+Console.WindowWidth = hossz;
+
+//ne látszódjon a cursor helye
+Console.CursorVisible = false;
+
+//karakter
+int sor = 0;
+int oszlop = 0;
+
+string karakterem =
+    """
+    (O.O)
+    """;
+
+Console.Write(karakterem);
+
+var gomb = Console.ReadKey();
