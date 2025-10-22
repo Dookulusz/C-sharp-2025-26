e1b865e94fe342347604a5854e525c91fa82b73d
Dookusz <daniel.doka@umszki.hu>
2025-10-22 10:19:42 +0200

    Órai munka

---
+++ Órai munkáim/For ciklus 2.0/Ciklus2.0/Ciklus2.0/Program.cs
@@ -0,0 +1,12 @@
+﻿// Bejárató ciklus
+
+string szoveg = "árvíztűrő tükörfúrógép";
+
+//ismételjük minden elemre a gyűjtemnényben
+//isméeljük minde betűre a szövegben
+
+foreach (char betu in szoveg)
+{
+    Console.WriteLine(betu);
+}
+
---
+++ Órai munkáim/For ciklus 2.0/Ciklus2.0/Cklus 2.0 gyakorlás/Program.cs
@@ -0,0 +1,30 @@
+﻿string magánhangzók = "aáeéoóuúiíöőüű";
+string mássalhangók = "qwrtzpsdfghjklyxcvbnm";
+string eredeti = "Tudsz így beszélni?";
+string hablagy = "tuvudsz ivigy bevesévélnivi?";
+
+/* SZABÁLY
+ * Járjuk körbe az eredeti szöveget beűnkét
+ * 
+ *      Füzzök hozzá a hablatyhoz
+ *      
+ *      Vizsgáljuk meg, hogy magánhangzó-e?
+ *      
+ *          Ha igen, akkor ismételjük meg egy V betűvel
+ *      
+ *      Elágazás vége
+ *      
+ */
+
+foreach (char betu in eredeti)
+{
+    if (magánhangzók.Contains(betu))
+    {
+        Console.WriteLine(betu + "\nv");
+        Console.WriteLine(betu);
+    }
+    else
+    {
+        Console.WriteLine(betu);
+    }
+}
