7d2cb3d41d85be4c8ec070cc73d9131236142965
Dookusz <daniel.doka@umszki.hu>
2025-11-11 10:45:14 +0100

    Tankönyvi megoldás

---
+++ Órai munkáim/Programozási tételek/óra1/óra1/Program.cs
@@ -0,0 +1,26 @@
+﻿/* adott egy tulajdonság (tetszőleges mennyiségű adat)
+ * Adott egy tulajdonság, ami az adatokon értelmezett.
+ * A feladat annak hatékony eldöntése, hogy
+ * tartalmaz-e ennek megfelelő elemet.
+ */
+
+//keressünk negatív számot
+int[] szamok = [10, 20, 30, -5, 50, 0, -10];
+
+//induljunk ki az első elemtől
+int i = 0;
+
+while (i < szamok.Length && szamok[i] >= 0)
+{
+    i++;
+}
+
+//mi a válasz?
+if (i == szamok.Length)
+{
+    Console.WriteLine("Nem volt ilyen");
+}
+else
+{
+    Console.WriteLine("volt");
+}
