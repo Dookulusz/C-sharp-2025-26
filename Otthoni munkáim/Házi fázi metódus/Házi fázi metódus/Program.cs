8140a66358a5bf0ba4f85e364100d76b06a15887
Dookusz <daniel.doka@umszki.hu>
2025-11-25 12:12:34 +0100

    Házi fázi

---
+++ Otthoni munkáim/Házi fázi metódus/Házi fázi metódus/Program.cs
@@ -0,0 +1,17 @@
+﻿/* 
+ * készítsünk függvényt, ami összeszoros két pozitív egész számot!
+ * A megvasítás során ne használjuk a '*' operátort!
+ * A szorzás igazából sokszorozást, ismételgetett összeadást jelent.
+ * Tipp Használjuk for() ciklust!
+ * 
+ * Készísünk egy függvényt, ami két pozitív egész számot elosztva megadja
+ * az osztás maradékát!
+ * De a megvalósítás során ne használjuk a % operátort
+ * 
+ * Készítsünk Minimum/maximum függvényt ami két szám közül
+ * kivlasztja a kissebbet illetve  a nagyobbat!
+ * 
+ * Készítsünk függvénít éskapcsolat illetve vagykapcsolat néven!
+ * ne használjuk hozzá az operátorokat (&& ||), helyette
+ * egyszerű elégazást használjunk (lásd igazságtábla)
+ */
